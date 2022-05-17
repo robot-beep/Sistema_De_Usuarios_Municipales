cd8906394652053a37466d554b14886e84add81c
Pedro Pablo Lopez Mena <pedro.lopez@alumnos.uv.cl>
2022-05-16 23:27:45 -0400

    commit de login admin

--- Componentes/admin/admin.controller.js
+++ Componentes/admin/admin.controller.js
@@ -21,7 +21,23 @@ async function createAdmin(admin){
     return await adminRepository.createAdmin(admin)
 }
 
+async function login(rut, password){
+    admin = await adminRepository.getRutAdmin(rut)
+    if(admin == []){
+        return "usuario o clave incorrecta";
+    }else{
+        if(admin[0].password == password){
+            return "si";
+        
+        }else{
+            return "usuario o clave incorrecta";
+        }
+    }
+
+}
+
 module.exports.getAdmins = getAdmins;
 module.exports.createAdmin = createAdmin;
 module.exports.getAdminByEmail = getAdminByEmail;
 module.exports.getAdminByRut = getAdminByRut;
+module.exports.login = login; 
