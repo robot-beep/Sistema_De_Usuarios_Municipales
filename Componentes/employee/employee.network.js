c0fedf7ecf4f4e35713a10de6e514a34f5579b01
Pedro Pablo Lopez Mena <pedro.lopez@alumnos.uv.cl>
2022-05-21 11:57:49 -0400

    agregado el sistema de login de employee, e implementación de logout en admin y employee [falta testing]

--- Componentes/employee/employee.network.js
+++ Componentes/employee/employee.network.js
@@ -2,66 +2,138 @@ const express = require('express')
 const router = express.Router();
 const controller = require("./employee.controller")
 const middleware = require('../../middleware')
+const jwt = require('jsonwebtoken');
 
 
-router.post('/employee/signup',middleware.verifyToken, async (req, res) =>{
+router.post('/employee/signup', middleware.verifyToken, async (req, res) => {
     const employee = req.body;
     await controller.createEmployee(employee);
-   
+
 })
 
-router.get('/employee/all', middleware.verifyToken,async (req, res)=>{
-    try {res.send((await (controller.getEmployees())));}
-    catch(error){
-        res.send(error.message,500)
+router.get('/employee/all', middleware.verifyToken, async (req, res) => {
+    try { res.send((await (controller.getEmployees()))); }
+    catch (error) {
+        res.send(error.message, 500)
     };
 });
 
 
-router.get('/employee/id/:id',middleware.verifyToken, async (req, res)=>{
+router.get('/employee/id/:id', middleware.verifyToken, async (req, res) => {
     const id = req.params["id"];
     res.send(await controller.getEmployeeById(id));
-    try {}catch(error){
-        res.send(error.message,500)
+    try { } catch (error) {
+        res.send(error.message, 500)
     };
 });
 
-router.get('/employee/department/:department',middleware.verifyToken, async (req, res)=>{
+router.get('/employee/department/:department', middleware.verifyToken, async (req, res) => {
     const department = req.params["department"];
-    try {res.send(await controller.getEmployeeByDepartment(department))}
-    catch(error){
-        res.send(error.message,500)
+    try { res.send(await controller.getEmployeeByDepartment(department)) }
+    catch (error) {
+        res.send(error.message, 500)
     };
 });
 
-router.get('/employee/rut/:rut',middleware.verifyToken, async (req, res)=>{
+router.get('/employee/rut/:rut', middleware.verifyToken, async (req, res) => {
     const rut = req.params["rut"];
-    try {res.send(await controller.getEmployeeByRut(rut))}
-    catch(error){
-        res.send(error.message,500)
+    try { res.send(await controller.getEmployeeByRut(rut)) }
+    catch (error) {
+        res.send(error.message, 500)
     };
 });
 
 
-router.patch('/employee/:id',middleware.verifyToken,async (req,res)=>{
+router.patch('/employee/:id', middleware.verifyToken, async (req, res) => {
     const id = req.params["id"];
     const employee = req.body;
-    try {res.send(await controller.updaterEmployee(id, employee))}catch(error){
-        res.send(error.message,500)
-    };  
+    try { res.send(await controller.updaterEmployee(id, employee)) } catch (error) {
+        res.send(error.message, 500)
+    };
 });
 
 
 
-router.delete('/employee/:id',middleware.verifyToken,async (req,res)=>{
+router.delete('/employee/:id', middleware.verifyToken, async (req, res) => {
     const id = req.params["id"];
-    try {res.send(await controller.deleteUser(id))}catch(error){
-        res.send(error.message,500);
-    };  
+    try { res.send(await controller.deleteUser(id)) } catch (error) {
+        res.send(error.message, 500);
+    };
+});
+
+//sesion
+
+router.post('/employee/signup', async (req, res) => {
+    const employee = req.body;
+    const rut = req.body.rut;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //primero reviso si es que existe algun usuario con el mismo email 
+    var testEmail = await controller.getAdminByEmail(email);
+    var testRut = await controller.getAdminByRut(rut);
+
+    if ((testEmail == "") && (testRut == "")) {
+        console.log(await controller.createAdmin(employee));
+
+        jwt.sign(rut, 'secret_key_employee', (err, token) => {
+            if (err) {
+                res.cookie('token', token, { maxAge: 60000 * 240 });
+                res.status(400).send({ msg: 'Error' })
+            }
+            else {
+                res.send({ msg: 'success', token: token })
+            }
+        })
+
+    } else {
+        res.send("el usuario ya existe")
+    }
+})
+
+
+//inicio de sesión 
+
+router.post('/employee/login/', async (req, res) => {
+    const rut = req.body.rut;
+    const password = req.body.password;
+    let permiso = await controller.login(rut, password);
+    if (permiso == "si") {
+
+        jwt.sign(rut, 'secret_key_employee', (err, token) => {
+            if (err) {
+                res.status(400).send({ msg: 'Error' })
+            }
+            else {
+                res.cookie('token', token, { maxAge: 60000 * 240 });
+                res.send({ msg: 'success', token: token });
+            }
+        })
+
+    } else {
+        res.send("el usuario ya existe")
+    }
+
+});
+
+//cerrar sesion
+
+router.post("/employee/logout", middleware.verifyToken, async (req, res) => {
+
+    const authHeader = await req.headers["token"];
+    jwt.sign(authHeader, "token", { expiresIn: 1 }, (logout, err) => {
+        if (logout) {
+            res.cookie('token', "token", { maxAge: 1});
+            res.send('Has sido desconectado' );
+        } else {
+            res.send('Error' );
+        }
+    });
 });
 
 
 
 
-module.exports =  router;
+
+module.exports = router;
 
--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
-const { verify } = require('jsonwebtoken');
+
+
+
 //verificación de tokens 
 function verifyToken(req, res, next) {
     const authHeader = req.headers["token"];
@@ -11,6 +13,18 @@ function verifyToken(req, res, next) {
     });
 }
 
+function verifyTokenEmployee(req, res, next) {
+    const authHeader = req.headers["token"];
+    if (authHeader == null) return res.sendStatus(403);
+    jwt.verify(authHeader, "secret_key", (err, user) => {
+        if (err) return res.sendStatus(404);
+        req.user = user;
+        next();
+    });
+}
+
+
+module.exports = {verifyTokenEmployee};
 module.exports = {verifyToken};
 
 
